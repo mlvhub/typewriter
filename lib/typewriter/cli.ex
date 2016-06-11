31232e4cc39cddfe22af2e8d0e47a7e5bbfdf065
Miguel Lopez V <mlopezva@akamai.com>
2016-06-11 12:36:15 -0600

    Add CLI option for input to stop or reload

--- lib/typewriter/cli.ex
+++ lib/typewriter/cli.ex
@@ -3,8 +3,9 @@ defmodule Typewriter.CLI do
   def main(args \\ []) do
     args
     |> parse_args
+    |> run
     |> response
-    |> output
+    |> handle_input
   end
 
   defp parse_args(args) do
@@ -12,16 +13,31 @@ defmodule Typewriter.CLI do
       args
       |> OptionParser.parse(switches: [port: :number])
 
-    {opts, List.to_string(project_path)}
+    {opts[:port], List.to_string(project_path)}
   end
 
-  defp response({opts, project_path}) do
-    IO.puts "#{opts[:port]}"
-    Typewriter.run(project_path, opts[:port])
+  def run({port, project_path}) do
+    IO.puts "#{port}"
+    Typewriter.run(project_path, port)
+    {port, project_path}
   end
 
-  def output(_result) do
-    :timer.sleep(:infinity)
+  defp response({port, project_path}) do
+    # Send response
+    {port, project_path}
   end
+
+  def handle_input(args)  do
+    input = IO.gets "What would you like to do? (s)top, (r)eload: "
+    handle_input(args, input |> String.strip)
+  end
+  def handle_input(args, "s"), do: handle_input(args, "stop")
+  def handle_input(args, "r"), do: handle_input(args, "reload")
+  def handle_input(_, "stop"), do: :ok
+  def handle_input(args, "reload") do
+    run(args)
+    handle_input(args)
+  end
+  def handle_input(args, _), do: handle_input(args)
 
 end
